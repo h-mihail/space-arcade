7ecc4d25db7379e282611973d8487f4df4406ccc
h-mihail <33.mihail@gmail.com>
2020-08-30 01:02:58 +0300

    Deleted unused assets

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,1 @@
 export { default as background } from "./background.png"
-export { default as ship1 } from "./ship1.png"
-export { default as ship2 } from "./ship2.png"
-export { default as ship3 } from "./ship3.png"
